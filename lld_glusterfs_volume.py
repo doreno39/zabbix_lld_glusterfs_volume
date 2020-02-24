e42c0d0a09dba586e161c1a821c395ddd973dc90
truongln <truongln@vng.com.vn>
2020-02-24 15:12:07 +0700

    add expect_vol.txt

--- lld_glusterfs_volume.py
+++ lld_glusterfs_volume.py
@@ -6,7 +6,20 @@
 
 from glustercmd import GlusterCommand
 
+def read_file_to_list(pathfile):
+    with open(pathfile, 'r') as f:
+    lines = f.readlines()
+    result = [item for item in [line.strip() for line in lines] if item]
+    return result
+
 if __name__ == "__main__":
+    except_vol_path_file = "except_vol.txt"
+    data = []
+
     cmd = GlusterCommand('gluster volume list', timeout=10)
     cmd.run()
-    print cmd.stdout
+    all_vol = cmd.stdout
+
+    except_vols = read_file_to_list(except_vol_path_file)
+    vols = (vol for vol in all_vol if not any(ignore in vol for ignore in except_vols))
+    print vols
