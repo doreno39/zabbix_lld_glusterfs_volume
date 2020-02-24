26f3b68a9ec1e035c1e677c057c9b2f0d53d31a9
truongln <truongln@vng.com.vn>
2020-02-24 11:15:24 +0700

    first commit, added lld_glusterfs_volume.py

---
+++ lld_glusterfs_volume.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Description: to get the volume quota of the GlusterFS push to Zabbix server.
+# Require: Python 2.7 or later
+# Author: TruongLN
+# Date: 20200224
+
+
+if __name__ == "__main__":
